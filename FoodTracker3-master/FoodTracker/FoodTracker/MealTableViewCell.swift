c36e5911a23746fcf02bda3bc31d33f285e44217
koirsuni2 <62085082+koirsuni2@users.noreply.github.com>
2020-03-11 22:33:13 -0400

    Add files via upload

---
+++ FoodTracker3-master/FoodTracker/FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  
+//  MealViewController.swift
+//  FoodTracker
+//
+//  Created by sunita sharma Koirala on 2/18/20.
+//  Copyright © 2020 sunita sharma Koirala. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
